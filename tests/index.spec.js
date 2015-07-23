255996f54a2eab9dd4cfb61727d7bfec7eac9b31
Aristides <aristides@Davids-Mac-mini.local>
2015-07-23 13:10:34 -0600

    updated Travis file

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -2,5 +2,6 @@ var test = require('tape')
 var server = require('../dist/index.js')
 
 test('basic test setup', function (assert) {
+	assert.plan(2)
  	assert.equal(1 + 1, 1 + 1, 'Test passed!')
 })
